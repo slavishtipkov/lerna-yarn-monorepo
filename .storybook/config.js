a674c65a9fa616d4b6a86dbd1dcdbf9f15752495
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-06-19 19:49:36 +0300

    Back to the begining

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from "@storybook/react";
-
-const req = require.context("../packages/components", true, /.story.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
