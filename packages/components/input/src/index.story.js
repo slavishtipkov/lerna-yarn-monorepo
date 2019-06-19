ed822c570821cdf8c9950acb25cb956613545074
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-06-19 13:32:01 +0300

    Update storybook stories imports

--- packages/components/input/src/index.story.js
+++ packages/components/input/src/index.story.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import Input from ".";
+import Input from "../src";
 
 storiesOf("Input", module).add("default", () => (
   <Input placeholder="Hello World!" />
